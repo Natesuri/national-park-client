233b7a5bdc7bc79bc0ae31c8734d033d6b966e57
Natesuri <natesuri@gmail.com>
2019-01-21 11:08:30 -0500

    Adds comments throughout the parks components
    Adds comments explaining certain methods and functionality in the
    parks components

--- src/parks/parksApi.js
+++ src/parks/parksApi.js
@@ -1,13 +1,21 @@
 import apiUrl from '../apiConfig'
 
-// const apiUrl = 'http://localhost:4741'
+// gets the parks from the NPS /parks api endpoint via the app's /exploreParks endpoint.
+// The /exploreParks endpoint recieves
+// either the user's favoriteParks list (if signed in and has a favoriteParks list),
+// the default parks list (if not signed in),
+// or a combination of both (if the user is signed in, but has fewer than 10 favorites.)
 
+// userList contains the _id for a user's favoriteParks if they have one.
 export const getAllParks = ( user ) => (
   user && user.userList
     ? fetch(apiUrl + '/exploreParks/' + user.userList)
     : fetch(apiUrl + '/exploreParks/' + '0')
 )
 
+// if the user has a userList (favoriteParks list)
+// then pass in the park that will be used to update the list.
+// otherwise create a userList with that item
 export const addToParks = (user, favorite) => (
 
   user.userList
